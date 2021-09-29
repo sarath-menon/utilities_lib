ff8c439c45a0f2f9749d670c8c2b4bd93f3e209d
smenon <smenon@ethz.ch>
2021-09-29 16:38:35 +0200

    removed math helper

--- math_helper/include/math_helper.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <math.h>
-
-// Constrains input value to a range
-constexpr static float limit(float val, const float max, const float min);
--- math_helper/src/math_helper.cpp
+++
@@ -1,9 +0,0 @@
-#include "math_helper.h"
-
-constexpr static float limit(float val, const float max, const float min) {
-  if (val > max)
-    val = max;
-  else if (val < min)
-    val = min;
-  return val;
-}
