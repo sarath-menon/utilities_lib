c868984525a921e27a53d2361f0584006388562d
smenon <smenon@ethz.ch>
2021-10-01 20:30:37 +0200

    changed direct logging -> add variable logging

--- logger/include/logger.h
+++ logger/include/logger.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "safety_checks.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/daily_file_sink.h"
 #include "spdlog/sinks/stdout_sinks.h"
@@ -6,7 +7,9 @@
 #include "string_helper.h"
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <string.h>
+#include <yaml-cpp/yaml.h>
 
 class Logger {
 
@@ -16,6 +19,9 @@ private:
 
   /// fstream for data logging
   std::fstream data_logger_;
+
+  /// Contains the addresses of variables to be logged
+  std::map<const std::string, const float *> log_address_book_;
 
 public:
   // Constructor to initialize logger
@@ -33,8 +39,23 @@ public:
   // Shutdown logger logger
   void shutdown();
 
-  // Shutdown data logger
+  /// Shutdown data logger
   void shutdown_data_logger();
+
+  /// To go to next line
+  void log_next_line();
+
+  /// Add variable to the log
+
+  // template <typename T> void add_log(T variable, T &address){};
+
+  void add_to_log(const std::string name, const float *const address);
+
+  void log_now();
+
+public:
+  // To load setpoints from yaml
+  void set_parameters(std::string path);
 
   // public:
   //   /// Getter function
--- logger/src/logger.cpp
+++ logger/src/logger.cpp
@@ -3,8 +3,8 @@
 Logger::Logger(std::string event_log_path, std::string data_log_path) {
 
   // Create folders to store log files
-  std::filesystem::create_directories("logs/data_logs");
-  std::filesystem::create_directories("logs/event_logs");
+  std::filesystem::create_directories(data_log_path);
+  std::filesystem::create_directories(event_log_path);
 
   // Initiaize event logger
   std::vector<spdlog::sink_ptr> sinks;
@@ -26,10 +26,39 @@ Logger::Logger(std::string event_log_path, std::string data_log_path) {
 
   const int buffer_size = sizeof(buffer) / sizeof(char);
   std::string file_name = string_helper::convertToString(buffer, buffer_size);
-  data_log_path = data_log_path + file_name;
+  std::string extension = "txt";
+
+  data_log_path = data_log_path + file_name + extension;
+
+  std::cout << "data log path:" << data_log_path << '\n';
 
   data_logger_.open(data_log_path,
                     std::ios::trunc | std::ios::out | std::ios::in);
+
+  // // Data logger headings
+  // // Vector of headings
+  // std::vector<std::string> headings = {
+  //     "x",
+  //     "y",
+  //     "z",
+  //     "roll",
+  //     "pitch",
+  //     "yaw",
+  //     "net_thrust",
+  //     "torque_x",
+  //     "torque_y",
+  //     "torque_z",
+  //     "motor_0_command",
+  //     "motor_1_command",
+  //     "motor_2_command",
+  //     "motor_3_command",
+  // };
+
+  // // Write headings
+  // for (int i = 0; i < headings.size(); i++) {
+  //   data_logger_ << headings[i] << '\t';
+  // }
+  // data_logger_ << std::endl;
 }
 
 void Logger::shutdown() {
@@ -46,4 +75,25 @@ void Logger::log_info(std::string text) { event_logger_->info(text); }
 
 void Logger::log_error(std::string text) { event_logger_->error(text); }
 
-void Logger::log_data(float data) { data_logger_ << data; }
+void Logger::log_data(float data) { data_logger_ << data << '\t'; }
+
+void Logger::log_next_line() { data_logger_ << '\n'; }
+
+void Logger::set_parameters(std::string path) {
+  // Safety check, see if file exists
+  safety_checks::yaml_file_check(path);
+
+  // Load yaml file containing quadcopter properties
+  YAML::Node logger_yaml = YAML::LoadFile(path);
+}
+
+void Logger::add_to_log(const std::string name, const float *const address) {
+  log_address_book_.insert({name, address});
+};
+
+void Logger::log_now() {
+
+  for (const auto &[key, value] : log_address_book_) {
+    data_logger_ << *value << '\t';
+  }
+}
