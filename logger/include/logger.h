3ce9572c18dee3647682aa69476a7bf7b58c578e
smenon <smenon@ethz.ch>
2021-10-01 11:44:21 +0200

    added functionaliy(still incomplete)

--- logger/include/logger.h
+++ logger/include/logger.h
@@ -3,17 +3,23 @@
 #include "spdlog/sinks/daily_file_sink.h"
 #include "spdlog/sinks/stdout_sinks.h"
 #include "spdlog/spdlog.h"
+#include "string_helper.h"
+#include <filesystem>
+#include <fstream>
 #include <string.h>
 
 class Logger {
 
 private:
   /// Shared pointer var for global access
-  std::shared_ptr<spdlog::logger> combined_logger_;
+  std::shared_ptr<spdlog::logger> event_logger_;
+
+  /// fstream for data logging
+  std::fstream data_logger_;
 
 public:
   // Constructor to initialize logger
-  Logger();
+  Logger(std::string event_log_path, std::string data_log_path);
 
   /// To log text information
   void log_info(std::string text);
@@ -21,8 +27,14 @@ public:
   /// To log text error
   void log_error(std::string error);
 
-  // Initialize data logger
-  void shutdown_logger();
+  /// To log text error
+  void log_data(float data);
+
+  // Shutdown logger logger
+  void shutdown();
+
+  // Shutdown data logger
+  void shutdown_data_logger();
 
   // public:
   //   /// Getter function
--- logger/src/logger.cpp
+++ logger/src/logger.cpp
@@ -1,20 +1,49 @@
 #include "logger.h"
 
-Logger::Logger() {
+Logger::Logger(std::string event_log_path, std::string data_log_path) {
+
+  // Create folders to store log files
+  std::filesystem::create_directories("logs/data_logs");
+  std::filesystem::create_directories("logs/event_logs");
+
   // Initiaize event logger
   std::vector<spdlog::sink_ptr> sinks;
   sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
   sinks.push_back(std::make_shared<spdlog::sinks::daily_file_sink_st>(
-      "logs/text-log.txt", 23, 59));
-  combined_logger_ =
-      std::make_shared<spdlog::logger>("name", begin(sinks), end(sinks));
+      event_log_path, 23, 59));
+  event_logger_ = std::make_shared<spdlog::logger>("event_logger", begin(sinks),
+                                                   end(sinks));
+  spdlog::register_logger(event_logger_);
+
+  // Initialize data logger
+
+  // Buffer to store file name
+  char buffer[80];
+  // Get current time
+  time_t t = std::time(nullptr);
+  // Set date+time as filename
+  strftime(buffer, sizeof(buffer), "%F %r.", std::localtime(&t));
+
+  const int buffer_size = sizeof(buffer) / sizeof(char);
+  std::string file_name = string_helper::convertToString(buffer, buffer_size);
+  data_log_path = data_log_path + file_name;
+
+  data_logger_.open(data_log_path,
+                    std::ios::trunc | std::ios::out | std::ios::in);
 }
 
-void Logger::shutdown_logger() {
+void Logger::shutdown() {
   // Initiaize event logger
+
+  data_logger_.close();
+  spdlog::drop("event_logger");
   spdlog::shutdown();
 }
 
-void Logger::log_info(std::string text) { combined_logger_->info(text); }
+void Logger::shutdown_data_logger() { data_logger_.close(); }
 
-void Logger::log_error(std::string text) { combined_logger_->error(text); }
+void Logger::log_info(std::string text) { event_logger_->info(text); }
+
+void Logger::log_error(std::string text) { event_logger_->error(text); }
+
+void Logger::log_data(float data) { data_logger_ << data; }
